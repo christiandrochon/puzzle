54a6a1709abd534e99cb9221d95f57a0ab11c998
christiandrochon <hello.christiandrochon.dev>
2026-04-29 17:31:36 +0200

    feat(app): connecter la page de jeu a la route racine

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { PageJeu } from './composant/jeu/page-jeu/page-jeu';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: PageJeu,
+  },
+];
--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,12 +1,21 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { App } from './app';
+import { PageJeu } from './composant/jeu/page-jeu/page-jeu';
 
 describe('App', () => {
+
+  // test du composant PageJeuComposant et PAS du composant App
+  let fixture: ComponentFixture<PageJeu>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [App],
+      imports: [App, PageJeu],
     }).compileComponents();
+
+    // creation du fixture pour PageJeu et pas pour App
+    fixture = TestBed.createComponent(PageJeu);
   });
+
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(App);
@@ -15,9 +24,29 @@ describe('App', () => {
   });
 
   it('should render title', async () => {
-    const fixture = TestBed.createComponent(App);
+    fixture.componentInstance.puzzle = {} as any;
+
+    fixture.detectChanges();
     await fixture.whenStable();
+
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, puzzle');
+    const title = compiled.querySelector('.titre-page');
+
+    expect(title).not.toBeNull();
+    expect(title!.textContent).toContain('Puzzle chenille');
   });
+
+
+  // it('should render title', async () => {
+  //   const fixture = TestBed.createComponent(App);
+  //   fixture.detectChanges();
+  //
+  //   await fixture.whenStable();
+  //
+  //   const compiled = fixture.nativeElement as HTMLElement;
+  //   const title = compiled.querySelector('h2');
+  //
+  //   expect(title).not.toBeNull();
+  //   expect(title!.textContent).toContain('Puzzle chenille');
+  // });
 });
